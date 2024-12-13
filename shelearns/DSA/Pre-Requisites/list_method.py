0b462b157cb0de031a93d337842a2af35330cc5d
CodeXena <anugyasaxena_cs24a02_006@dtu.ac.in>
2024-12-13 15:57:41 +0530

    Add files via upload

--- shelearns/DSA/Pre-Requisites/list_method.py
+++ shelearns/DSA/Pre-Requisites/list_method.py
@@ -3,14 +3,19 @@ list=[1,'gigi',2.9,3,4,4,5]
 kit=[23,"minion","yui",78]
 lit=[list,kit] 
 
-list.append(34)
+del list[2]
+print(list)
+
+
+
+list.append(67)
 print(list)
 
 list.insert(2,'qwe')
 print(list)
 
 
-list.pop(0) #with index removes last element
+list.pop(0) #without index removes last element..pop(index)
 print(list)
 
 copy=list.copy() 
@@ -22,7 +27,7 @@ print(copy)
 l=list.count(4)
 print(l)
 
-list.remove('gigi')
+list.remove('gigi')#removes only first occurence
 list.remove('qwe')
 print(list)
 
@@ -38,6 +43,10 @@ print(list)
 list.sort()
 
 list.clear()
+
+#The enumerate() function allows you to loop through a list (or any iterable) while keeping track of the index.
+for index,list in enumerate(list):
+    print(index,list)
 
 
 print(list,kit,lit)
